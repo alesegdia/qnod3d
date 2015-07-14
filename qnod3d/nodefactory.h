0e7194ffc8f80f68bb272b6a8aadba0b0a84ab10
Alejandro Seguí <alesegdia@gmail.com>
2015-07-14 19:23:28 +0200

    adding getModelMatrix() method to Node; adding position to Node; adding collectModels() method to NodeFactory

--- qnod3d/nodefactory.h
+++ qnod3d/nodefactory.h
@@ -2,14 +2,29 @@
 #define NODEFACTORY_H
 
 #include <QVector2D>
+#include <QMatrix4x4>
 #include <QList>
 
 class Node
 {
 public:
-    Node(int w, int h) : m_size(w,h) {}
-    Node(QVector2D size) : m_size(size) {}
+    Node(int x, int y, int w, int h)
+        : m_size(x,y), m_position(w,h)
+    {}
+
+    Node(QVector2D size, QVector2D position)
+        : m_size(size), m_position(position)
+    {}
+
+    QMatrix4x4 getModelMatrix()
+    {
+        QMatrix4x4 model;
+        model.translate( m_position.x(), m_position.y(), 0.0 );
+        return model;
+    }
+
     QVector2D m_size;
+    QVector2D m_position;
 };
 
 class NodeFactory
@@ -23,10 +38,18 @@ public:
         }
     }
 
-    Node* makeNode( int w, int h ) {
-        Node* n = new Node(w,h);
+    Node* makeNode( int x, int y, int w, int h ) {
+        Node* n = new Node(x,y,w,h);
         m_nodeList.append(n);
         return n;
+    }
+
+    QList<QMatrix4x4>&& collectModels() {
+        QList<QMatrix4x4> models;
+        for( QList<Node*>::iterator it = m_nodeList.begin(); it != m_nodeList.end(); it++) {
+            models.append((*it)->getModelMatrix());
+        }
+        return std::move(models);
     }
 
 private:
