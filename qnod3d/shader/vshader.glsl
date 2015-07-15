5fad4b267134d439fd466649163534ff7b96d5c5
Alejandro Seguí <alesegdia@gmail.com>
2015-07-15 03:59:50 +0200

    using proper MVP computation

--- qnod3d/shader/vshader.glsl
+++ qnod3d/shader/vshader.glsl
@@ -3,7 +3,8 @@ precision mediump int;
 precision mediump float;
 #endif
 
-uniform mat4 mvp_matrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
 uniform vec2 node_size;
 
 in vec4 a_position;
@@ -15,7 +16,6 @@ varying vec2 v_texcoord;
 void main()
 {
     a_position.xy = a_position.xy * node_size;
-    gl_Position = a_modelMatrix * mvp_matrix * a_position;
-
+    gl_Position = projectionMatrix * viewMatrix * a_modelMatrix * a_position;
     v_texcoord = a_texcoord;
 }
