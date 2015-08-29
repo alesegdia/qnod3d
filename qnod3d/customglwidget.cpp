9b244328a29613c49864f38678cb26d123d6a15a
Alejandro Seguí <alesegdia@gmail.com>
2015-08-29 20:23:58 +0200

    style refactoring

--- qnod3d/customglwidget.cpp
+++ qnod3d/customglwidget.cpp
@@ -108,11 +108,17 @@ void CustomGLWidget::initTextures()
 void CustomGLWidget::mousePressEvent(QMouseEvent* e)
 {
     m_pressed = true;
-    if( e->button() == Qt::MouseButton::RightButton ) {
+
+    if( e->button() == Qt::MouseButton::RightButton )
+    {
         m_toolMode = ToolMode::SCN_SCALE;
-    } else if( e->button() == Qt::MouseButton::LeftButton ) {
+    }
+    else if( e->button() == Qt::MouseButton::LeftButton )
+    {
         m_toolMode = ToolMode::SCN_TRANSLATE;
-    } else if( e->button() == Qt::MouseButton::MidButton ) {
+    }
+    else if( e->button() == Qt::MouseButton::MidButton )
+    {
         m_toolMode = ToolMode::SCN_ROTATE;
     }
     m_mousePressPosition = QVector2D(e->localPos());
@@ -120,23 +126,30 @@ void CustomGLWidget::mousePressEvent(QMouseEvent* e)
 
 void CustomGLWidget::mouseMoveEvent(QMouseEvent *e)
 {
-    if( m_pressed ) {
+    if( m_pressed )
+    {
         QVector2D deltaMousePos = QVector2D( e->localPos() ) - m_mousePressPosition;
         deltaMousePos.setY( -deltaMousePos.y() );
-        switch( m_toolMode ) {
+
+        switch( m_toolMode )
+        {
+
         case ToolMode::SCN_ROTATE:
             m_rotation_x *= QQuaternion::fromEulerAngles(
                         QVector3D(   0.f, deltaMousePos.x(), 0.f ) );
             m_rotation_y *= QQuaternion::fromEulerAngles(
                         QVector3D( - deltaMousePos.y(), 0.f, 0.f ) );
             break;
+
         case ToolMode::SCN_TRANSLATE:
             m_position += deltaMousePos / 40;
             break;
+
         case ToolMode::SCN_SCALE:
             m_scale += deltaMousePos.y() * m_scale / 100.f;
             m_scale = qMax( 0.f, m_scale );
             break;
+
         }
 
         m_mousePressPosition = QVector2D( e->localPos() );
