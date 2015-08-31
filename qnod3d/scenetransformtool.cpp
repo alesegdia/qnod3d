c9b62791a2c876e6510fea61536cc87b374ae827
Alejandro Seguí <alesegdia@gmail.com>
2015-08-31 03:26:23 +0200

    adding scene transform action in scene transform tool

--- qnod3d/scenetransformtool.cpp
+++ qnod3d/scenetransformtool.cpp
@@ -1,7 +1,63 @@
-#include "cameratransformtool.h"
+#include "scenetransformtool.h"
 
-CameraTransformTool::CameraTransformTool()
+SceneTransformTool::SceneTransformTool(SceneTransform &sceneTransform)
+    : m_sceneTransform(sceneTransform)
 {
 
 }
 
+void SceneTransformTool::mousePressEvent(QMouseEvent *e)
+{
+    m_pressed = true;
+
+    if( e->button() == Qt::MouseButton::RightButton )
+    {
+        m_toolMode = ToolMode::SCN_SCALE;
+    }
+    else if( e->button() == Qt::MouseButton::LeftButton )
+    {
+        m_toolMode = ToolMode::SCN_TRANSLATE;
+    }
+    else if( e->button() == Qt::MouseButton::MidButton )
+    {
+        m_toolMode = ToolMode::SCN_ROTATE;
+    }
+    m_mousePressPosition = QVector2D(e->localPos());
+}
+
+void SceneTransformTool::mouseReleaseEvent(QMouseEvent *e)
+{
+    m_pressed = false;
+}
+
+void SceneTransformTool::mouseMoveEvent(QMouseEvent *e)
+{
+    if( m_pressed )
+    {
+        QVector2D deltaMousePos = QVector2D( e->localPos() ) - m_mousePressPosition;
+        deltaMousePos.setY( -deltaMousePos.y() );
+
+        switch( m_toolMode )
+        {
+
+        case ToolMode::SCN_ROTATE:
+            m_sceneTransform.m_rotation_x *= QQuaternion::fromEulerAngles(
+                        QVector3D(   0.f, deltaMousePos.x(), 0.f ) );
+            m_sceneTransform.m_rotation_y *= QQuaternion::fromEulerAngles(
+                        QVector3D( - deltaMousePos.y(), 0.f, 0.f ) );
+            break;
+
+        case ToolMode::SCN_TRANSLATE:
+            m_sceneTransform.m_position += deltaMousePos / 40;
+            break;
+
+        case ToolMode::SCN_SCALE:
+            m_sceneTransform.m_scale += deltaMousePos.y() * m_sceneTransform.m_scale / 100.f;
+            m_sceneTransform.m_scale = qMax( 0.f, m_sceneTransform.m_scale );
+            break;
+
+        }
+
+        m_mousePressPosition = QVector2D( e->localPos() );
+    }
+}
--- qnod3d/scenetransformtool.h
+++ qnod3d/scenetransformtool.h
@@ -1,11 +1,33 @@
 #ifndef CAMERATRANSFORMTOOL_H
 #define CAMERATRANSFORMTOOL_H
 
+#include <QVector2D>
 
-class CameraTransformTool
+#include "itool.h"
+#include "scenetransform.h"
+
+enum ToolMode {
+    SCN_TRANSLATE,
+    SCN_ROTATE,
+    SCN_SCALE
+};
+
+class SceneTransformTool : public ITool
 {
 public:
-    CameraTransformTool();
+    SceneTransformTool( SceneTransform& sceneTransform );
+
+    void mousePressEvent(QMouseEvent *e) override;
+    void mouseReleaseEvent(QMouseEvent *e) override;
+    void mouseMoveEvent(QMouseEvent *e) override;
+
+private:
+    SceneTransform& m_sceneTransform;
+    bool m_pressed;
+    ToolMode m_toolMode;
+    QVector2D m_mousePressPosition;
+
 };
+
 
 #endif // CAMERATRANSFORMTOOL_H
